a2d0afc6feb246a4996f8b1d5798aec6c4c35763
Brett Smith <brett@logonbox.com>
2025-03-06 14:11:18 +0000

    Start next development.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 open module org.freedesktop.dbus.transport.ssh {
 	exports com.logonbox.dbus.transport.ssh;
     requires transitive com.sshtools.synergy.jdk16.client;
-    requires org.freedesktop.dbus; 
+    requires transitive org.freedesktop.dbus; 
     provides org.freedesktop.dbus.spi.transport.ITransportProvider
             with
             com.logonbox.dbus.transport.ssh.SshTransportProvider;
